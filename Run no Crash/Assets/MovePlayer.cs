2f45f615d75818b6fbb75471c444cb1165a03e08
Mohammad-537 <66563296+Mohammad-537@users.noreply.github.com>
2022-07-01 02:33:58 +0300

    add-jumb

--- Run no Crash/Assets/MovePlayer.cs
+++ Run no Crash/Assets/MovePlayer.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class MovePlayer : MonoBehaviour
 {
+    public Rigidbody rb;
+    public float jumbForce = 10;
     
     // Start is called before the first frame update
     void Start()
@@ -15,6 +17,14 @@ public class MovePlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(transform.position.y <= 0)
+            {
+                rb.AddForce(Vector3.up * jumbForce, ForceMode.Impulse);
+            }
+            
+        }
         if (Input.GetKeyDown(KeyCode.D))
         {
             if (transform.position.x < 7.5f)
